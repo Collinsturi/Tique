0e3ff76846310525f35a0a046667e6eff57897ea
collinsturi <collinsturi@gmail.com>
2025-07-18 11:13:11 +0300

    Feat: Added the router analytics router

---
+++ src/components/analytics/analytics.router.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { AnalyticsController } from "./analytics.controller";
+import {asyncHandler} from "../utils/asyncHandler";
+
+const router = express.Router();
+const controller = new AnalyticsController();
+
+router.get("/admin/summary", asyncHandler(controller.adminDashboard));
+router.get("/platform/summary", asyncHandler(controller.getPlatformSummary));
+router.get("/platform/monthly-trends", asyncHandler(controller.getMonthlySalesTrends));
+router.get("/platform/top-events", asyncHandler(controller.getTopSellingEvents));
+router.get("/platform/ticket-scan-status", asyncHandler(controller.getOverallTicketScanStatus));
+
+router.get("/event/:eventId/summary", asyncHandler(controller.getEventTicketSummary));
+router.get("/event/:eventId/scan-log", asyncHandler(controller.getEventScanLog));
+router.get("/event/:eventId/scan-status", asyncHandler(controller.getEventScanStatus));
+router.get("/event/:eventId/ticket-distribution", asyncHandler(controller.getTicketTypeDistribution));
+
+router.get("/organizer/wallet", asyncHandler(controller.getOrganizerEarningsSummary));
+router.get("/organizer/revenue", asyncHandler(controller.getRevenuePerEvent));
+
+export default router;
